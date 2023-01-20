3ab41271023fe9946b363e4cfdaad675e18ea0b2
Sutton, DeRon (ms449e) <ms449e@att.com>
2023-01-19 19:58:29 -0600

    Initial mongo db setup

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const Post = require('./models/post')
+const mongoose = require('mongoose');
 
 const app = express();
-
+mongoose.connect("mongodb+srv://dsutton1080:dsuttonAdmin@cluster0.bms6bfv.mongodb.net/?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to database!');
+  })
+  .catch(() => {
+    console.log('Connection failed!');
+  })
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false})); //not needed but doesn't hurt
 
@@ -14,7 +22,10 @@ app.use((req, res, next) => {
 });
 
 app.post("/api/posts", (req, res, next) => {
-  const post = req.body;
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content
+  });
   console.log(post);
   res.status(201).json({
     message: 'Post added successfully'
---
+++ backend/models/post.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+
+const postSchema = mongoose.Schema({   // Schema is a blueprint, need to use a model to do actual work
+  title: { type: String, required: true },
+  content: { type: String, required: true }
+});
+
+module.exports = mongoose.model('Post', postSchema);
